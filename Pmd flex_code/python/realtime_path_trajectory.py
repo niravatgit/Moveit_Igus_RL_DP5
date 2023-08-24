e54d437c914ae7d825eb9f7c47e7bcc114ef1df8
Nagaraj Pandiyan GNP <53268182+Nagarajpandiyan@users.noreply.github.com>
2023-08-24 11:57:16 +0530

    Update realtime_path_trajectory.py
    In this the Rviz displaying the point cloud we can visualize in this code the doom points are displaying the topic /royale_cam0/point_cloud  subscriber code off it .and then the region has select properly on it and it can able display properly so,the previous method solved. 
    
    Next Task:now we working on to select the points to make spline curve and make path planning

--- Pmd flex_code/python/realtime_path_trajectory.py
+++ Pmd flex_code/python/realtime_path_trajectory.py
@@ -4,15 +4,28 @@ from sensor_msgs.msg import PointCloud2
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
-import random
 
 # Define the region of interest (ROI) limits
-ROI_X_MIN = -0.1  # Minimum x-coordinate of the ROI
-ROI_X_MAX = 0.1 # Maximum x-coordinate of the ROI
-ROI_Y_MIN = -0.1  # Minimum y-coordinate of the ROI
-ROI_Y_MAX = 0.1 # Maximum y-coordinate of the ROI
-ROI_Z_MIN = -5  # Minimum z-coordinate of the ROI (for filtering out)
-ROI_Z_MAX = 5    # Maximum z-coordinate of the ROI (for filtering out)
+ROI_X_MIN = -0.2  # Minimum x-coordinate of the ROI
+ROI_X_MAX = 0.2   # Maximum x-coordinate of the ROI
+ROI_Y_MIN = -0.2  # Minimum y-coordinate of the ROI
+ROI_Y_MAX = 0.2   # Maximum y-coordinate of the ROI
+ROI_Z_MIN = 0.2   # Minimum z-coordinate of the ROI
+ROI_Z_MAX = 1.5   # Maximum z-coordinate of the ROI
+
+# Reference point coordinates for distance calculation
+REFERENCE_X = 0.0
+REFERENCE_Y = 0.0
+REFERENCE_Z = 0.0
+
+# Maximum distance from the reference point for visualization
+MAX_DISTANCE = 1.2
+
+def calculate_distance(x, y, z):
+    return np.sqrt((x - REFERENCE_X)**2 + (y - REFERENCE_Y)**2 + (z - REFERENCE_Z)**2)
+
+def normalize_distance(dist):
+    return (dist - 0) / (MAX_DISTANCE - 0)  # Normalize to [0, 1] range
 
 def point_cloud_callback(msg):
     # Extract point cloud data
@@ -41,34 +54,30 @@ def point_cloud_callback(msg):
     rotated_y = [-val for val in filtered_y]
     rotated_z = [-val for val in filtered_z]
 
-    # Create 3D scatter plot
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
+    # Calculate distances from reference point and normalize
+    distances = [calculate_distance(x_val, y_val, z_val) for x_val, y_val, z_val in zip(rotated_x, rotated_y, rotated_z)]
+    normalized_distances = [normalize_distance(dist) for dist in distances]
 
-    # Visualize points in red
-    ax.scatter(rotated_x, rotated_y, rotated_z, c='red', marker='o')
-
-    # Select random points based on distance
-    num_selected = min(len(rotated_x), 20)  # Select a maximum of 20 points
-    selected_indices = random.sample(range(len(rotated_x)), num_selected)
+    # Filter points within the desired distance range
+    selected_indices = [i for i, dist in enumerate(distances) if dist <= MAX_DISTANCE]
     selected_x = [rotated_x[i] for i in selected_indices]
     selected_y = [rotated_y[i] for i in selected_indices]
     selected_z = [rotated_z[i] for i in selected_indices]
+    selected_distances = [normalized_distances[i] for i in selected_indices]
 
-    # Visualize selected points in blue
-    ax.scatter(selected_x, selected_y, selected_z, c='blue', marker='o')
+    # Create 3D scatter plot
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    scatter = ax.scatter(selected_x, selected_y, selected_z, c=selected_distances, cmap='viridis', marker='o')
 
-    # Draw a simple trajectory path using lines
-    for i in range(num_selected - 1):
-        ax.plot([selected_x[i], selected_x[i+1]],
-                [selected_y[i], selected_y[i+1]],
-                [selected_z[i], selected_z[i+1]], c='blue')
+    # Add color bar to show normalized distance values
+    plt.colorbar(scatter, ax=ax, label='Normalized Distance from Reference Point')
 
     # Set labels and title
     ax.set_xlabel('X')
     ax.set_ylabel('Y')
     ax.set_zlabel('Z')
-    plt.title('Point Cloud Visualization with 180-degree Rotation along X-Axis')
+    plt.title('Point Cloud Visualization with 180-degree Rotation along X-Axis (Within {} m from Reference)'.format(MAX_DISTANCE))
 
     # Show the plot
     plt.show()
